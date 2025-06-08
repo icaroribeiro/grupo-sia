7d019e72c41543a1c07f8d6181e8313e27ec5c1c
icaroribeiro <icaroribeiro@hotmail.com>
2025-06-08 16:46:51 -0300

    Update tasks

--- desafio-18-06-2025/ai_agents_crew/tasks/generate_report_task.py
+++ desafio-18-06-2025/ai_agents_crew/tasks/generate_report_task.py
@@ -6,10 +6,11 @@ class GenerateReportTask:
         return Task(
             description=f"""
                 Based on the analysis summary: '{analysis_summary}', create an objective response
-                acknowledging the original user query: '{user_query}'.
+                acknowledging the original user's query: '{user_query}'.
             """,
             expected_output="""
                 A straightforward response summarizing the analysis findings for the user.
+                The response must always be written in the same language as the user's query.
             """,
             agent=agent,
         )
