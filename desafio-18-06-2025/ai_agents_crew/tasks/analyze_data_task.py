f8fc31aa06e0e0223b2cb57116dde4d326c2e84a
icaroribeiro <icaroribeiro@hotmail.com>
2025-06-08 23:36:55 -0300

    Update variable name

--- desafio-18-06-2025/ai_agents_crew/tasks/analyze_data_task.py
+++ desafio-18-06-2025/ai_agents_crew/tasks/analyze_data_task.py
@@ -2,10 +2,12 @@ from crewai import Agent, Task
 
 
 class AnalyzeDataTask:
-    def create(self, user_query: str, available_dfs_str: str, agent: Agent) -> Task:
+    def create(
+        self, user_query: str, available_dataframes_str: str, agent: Agent
+    ) -> Task:
         return Task(
             description=f"""
-                Analyze the available dataframes ({available_dfs_str}) to answer the following user's query: '{user_query}'.
+                Analyze the available dataframes ({available_dataframes_str}) to answer the following user's query: '{user_query}'.
                 Use the provided pandas tools to extract relevant information. 
                 Summarize your findings clearly and concisely, highlighting key observations and any relevant statistics.
                 Explicitly mention which dataframe(s) you are referring to for each finding.
