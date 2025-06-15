9285efee2cfb2fd55609689e4cd58f0e323ed9e4
icaroribeiro <icaroribeiro@hotmail.com>
2025-06-15 17:25:15 -0300

    Update file unzipping crew

--- desafio-18-06-2025/ai_agents_crew/crews/file_unzipping_crew.py
+++ desafio-18-06-2025/ai_agents_crew/crews/file_unzipping_crew.py
@@ -1,6 +1,4 @@
-from typing import List
-
-from crewai import LLM, Crew, Process, Task
+from crewai import LLM, Crew, Process
 
 from ai_agents_crew.agents.file_unzipper_agent import FileUnzipperAgent
 from ai_agents_crew.tasks.unzip_file_task import UnzipFileTask
@@ -11,26 +9,22 @@ class FileUnzippingCrew:
     def __init__(self, llm: LLM):
         self.__llm = llm
 
-    def kickoff_crew(self, zip_files_to_process: List[str], extracted_dir: str) -> Crew:
+    def kickoff_crew(self, zip_file_to_process: str, extracted_dir: str) -> Crew:
         unzip_file_tool = UnzipFileTool()
 
         file_unzipper_agent = FileUnzipperAgent(llm=self.__llm).create(
             tools=[unzip_file_tool]
         )
 
-        unzip_file_tasks: List[Task] = list()
-        for zip_file_to_process in zip_files_to_process:
-            unzip_file_tasks.append(
-                UnzipFileTask().create(
-                    zip_file_to_process=zip_file_to_process,
-                    extracted_dir=extracted_dir,
-                    agent=file_unzipper_agent,
-                )
-            )
+        unzip_file_task = UnzipFileTask().create(
+            zip_file_to_process=zip_file_to_process,
+            extracted_dir=extracted_dir,
+            agent=file_unzipper_agent,
+        )
 
         crew = Crew(
             agents=[file_unzipper_agent],
-            tasks=unzip_file_tasks,
+            tasks=[unzip_file_task],
             process=Process.sequential,
             verbose=True,
         )
