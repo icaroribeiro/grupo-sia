12c3352f5923058de3bcc160d12903b7035c1006
icaroribeiro <icaroribeiro@hotmail.com>
2025-06-22 20:23:23 -0300

    Update Gemini LLM name

--- desafio-24-06-2025/ai_agents_crew/llms/gemini_llm.py
+++ desafio-24-06-2025/ai_agents_crew/llms/gemini_llm.py
@@ -9,7 +9,7 @@ load_dotenv()
 class GeminiLLM:
     def create(self) -> LLM:
         return LLM(
-            model="gemini-2.0-flash",
+            model="gemini/gemini-2.0-flash",
             temperature=0.2,
             api_key=os.environ["GEMINI_API_KEY"],
         )
