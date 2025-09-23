e8a84d718cd6182e26e0ae361f83d3ec7048d7c6
icaroribeiro <icaroribeiro@hotmail.com>
2025-09-22 21:49:50 -0300

    Launch streamlit application

---
+++ desafio-2025-10-01/launch_streamlit_app.py
@@ -0,0 +1,28 @@
+import subprocess
+
+from src.layers.core_logic_layer.logging import logger
+from src.layers.core_logic_layer.settings.streamlit_app_settings import (
+    StreamlitAppSettings,
+)
+
+if __name__ == "__main__":
+    try:
+        streamlit_app_settings = StreamlitAppSettings()
+        subprocess.run(
+            [
+                "streamlit",
+                "run",
+                "src/main.py",
+                "--server.address",
+                f"{streamlit_app_settings.host}",
+                "--server.port",
+                f"{streamlit_app_settings.port}",
+            ],
+            check=True,
+        )
+    except KeyboardInterrupt:
+        message = "Streamlit client closed due to KeyboardInterrupt"
+        logger.error(message)
+    except Exception as error:
+        message = f"Error: Failed to initiate Streamlit client: {error}"
+        logger.error(message)
