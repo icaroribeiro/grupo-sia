25ef307569849ebc3c99144683350143997d7d97
icaroribeiro <icaroribeiro@hotmail.com>
2025-10-03 20:11:25 -0300

    Add application core logic layer

---
+++ projeto-final-2025-10-30/src/layers/core_logic_layer/settings/streamlit_app_settings.py
@@ -0,0 +1,19 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class StreamlitAppSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_prefix="STREAMLIT_APP_",
+        env_file_encoding="utf-8",
+        extra="ignore",
+        env_ignore_extra=True,
+    )
+
+    host: str = Field(default="localhost")
+    port: int = Field(default=8501)
+    output_data_dir_path: str = Field(default="data/output")
+    upload_data_dir_path: str = Field(default="data/upload")
+    upload_extracted_data_dir_path: str = Field(default="data/upload/extracted")
+    ingestion_data_dir_path: str = Field(default="data/ingestion")
