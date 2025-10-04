eca0a5e2bcd841abb6fa65dc55b89161453660aa
icaroribeiro <icaroribeiro@hotmail.com>
2025-10-03 23:22:47 -0300

    Update application container

--- desafio-2025-10-08/src/layers/core_logic_layer/container/container.py
+++ desafio-2025-10-08/src/layers/core_logic_layer/container/container.py
@@ -6,6 +6,12 @@ from src.layers.business_layer.ai_agents.agents.data_analysis_agent import (
 from src.layers.business_layer.ai_agents.agents.supervisor_agent import SupervisorAgent
 from src.layers.business_layer.ai_agents.agents.unzip_file_agent import UnzipFileAgent
 from src.layers.business_layer.ai_agents.llm.llm import LLM
+from src.layers.business_layer.ai_agents.tools.data_analysis_handoff_tool import (
+    DataAnalysisHandoffTool,
+)
+from src.layers.business_layer.ai_agents.tools.generate_distribution_tool import (
+    GenerateDistributionTool,
+)
 from src.layers.business_layer.ai_agents.tools.unzip_zip_file_tool import (
     UnzipZipFileTool,
 )
@@ -23,41 +29,46 @@ from src.layers.data_access_layer.postgresql.postgresql import PostgreSQL
 
 class Container(containers.DeclarativeContainer):
     ai_settings = providers.Singleton(AISettings)
+    llm = providers.Singleton(LLM, ai_settings=ai_settings)
 
     streamlit_app_settings = providers.Singleton(StreamlitAppSettings)
 
     postgresql_settings = providers.Singleton(PostgreSQLSettings)
-
-    llm = providers.Singleton(LLM, ai_settings=ai_settings)
-
-    unzip_zip_file_tool = providers.Singleton(UnzipZipFileTool)
-
     postgresql = providers.Singleton(
         PostgreSQL, postgresql_settings=postgresql_settings
     )
 
+    unzip_zip_file_tool = providers.Singleton(UnzipZipFileTool)
+    generate_distribution_tool = providers.Singleton(GenerateDistributionTool)
     unzip_file_agent = providers.Singleton(
         UnzipFileAgent,
         chat_model=llm.provided.chat_model,
     )
-
     data_analysis_agent = providers.Singleton(
         DataAnalysisAgent,
         chat_model=llm.provided.chat_model,
     )
-
     supervisor_agent = providers.Singleton(
         SupervisorAgent,
         chat_model=llm.provided.chat_model,
     )
-
+    delegate_to_unzip_file_agent_tool = providers.Singleton(
+        DataAnalysisHandoffTool,
+        agent_name=unzip_file_agent.provided.name,
+    )
+    delegate_to_data_analysis_agent_tool = providers.Singleton(
+        DataAnalysisHandoffTool,
+        agent_name=data_analysis_agent.provided.name,
+    )
     data_analysis_workflow = providers.Singleton(
         DataAnalysisWorkflow,
-        streamlit_app_settings=streamlit_app_settings,
+        unzip_zip_file_tool=unzip_zip_file_tool,
         unzip_file_agent=unzip_file_agent,
         data_analysis_agent=data_analysis_agent,
         supervisor_agent=supervisor_agent,
-        unzip_zip_file_tool=unzip_zip_file_tool,
+        delegate_to_unzip_file_agent_tool=delegate_to_unzip_file_agent_tool,
+        delegate_to_data_analysis_agent_tool=delegate_to_data_analysis_agent_tool,
+        eda_tools=[generate_distribution_tool],
     )
 
     workflow_runner = providers.Singleton(
