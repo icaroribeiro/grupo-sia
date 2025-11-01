2195dbbbe6792666f7b4575345844a1e08fcbe3e
icaroribeiro <icaroribeiro@hotmail.com>
2025-11-01 18:11:01 -0300

    Remove projeto-final-2025-10-30 folder

--- projeto-final-2025-10-30/src/layers/business_layer/ai_agents/agents/base_agent.py
+++
@@ -1,8 +0,0 @@
-from langchain_core.language_models import BaseChatModel
-from pydantic import BaseModel
-
-
-class BaseAgent(BaseModel):
-    name: str
-    prompt: str
-    chat_model: BaseChatModel
--- projeto-final-2025-10-30/src/layers/business_layer/ai_agents/models/base_state_graph_model.py
+++
@@ -1,9 +0,0 @@
-from langgraph.graph import StateGraph
-from pydantic import BaseModel, ConfigDict
-
-
-class BaseStateGraphModel(BaseModel):
-    name: str
-    graph: StateGraph
-
-    model_config = ConfigDict(arbitrary_types_allowed=True)
