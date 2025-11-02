7b18694b7c5739fcbe6d6d94b6916574510e9202
icaroribeiro <icaroribeiro@hotmail.com>
2025-11-02 08:56:59 -0300

    Add AI models

---
+++ projeto-final-2025-11-06/src/ai/models/invoice_mgmt_state_model.py
@@ -0,0 +1,8 @@
+from src.ai.models.base_state_model import (
+    BaseStateModel,
+)
+
+
+class InvoiceMgmtStateModel(BaseStateModel):
+    ingestion_args_list: list[dict[str, str]] | None
+    chart_data: dict | None = None
