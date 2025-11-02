f5f97dfa2db81feaad79e436ea271d151cf7f637
icaroribeiro <icaroribeiro@hotmail.com>
2025-11-02 08:59:51 -0300

    Add streamlit application and application error

---
+++ projeto-final-2025-11-06/src/streamlit_app.py
@@ -0,0 +1,77 @@
+import uuid
+
+import streamlit as st
+
+from src.core.logging import logger
+from src.presentation.pages.about_page import AboutPage
+from src.presentation.pages.chat_page import ChatPage
+from src.presentation.pages.data_analysis_page import (
+    DataAnalysisPage,
+)
+from src.presentation.pages.data_modeling_page import (
+    DataModelingPage,
+)
+from src.presentation.pages.home_page import HomePage
+from src.presentation.pages.invoice_ingestion_page import (
+    InvoiceIngestionPage,
+)
+
+
+class App:
+    def __init__(self) -> None:
+        st.sidebar.title("Navegação")
+        if "session_thread_id" not in st.session_state:
+            st.session_state.session_thread_id = str(uuid.uuid4())
+            logger.info(
+                f"New session started with thread_id: {st.session_state.session_thread_id}"
+            )
+        self.__setup_pages()
+
+    def __setup_pages(self) -> None:
+        self.__pages = {
+            "home": {
+                "title": "Ínicio",
+                "func": HomePage().show,
+            },
+            "data_modeling": {
+                "title": "Modelagem de Dados",
+                "func": DataModelingPage().show,
+            },
+            "invoice_ingestion": {
+                "title": "Ingestão de NF-e",
+                "func": InvoiceIngestionPage().show,
+            },
+            "data_analysis": {
+                "title": "Análise de Dados",
+                "func": DataAnalysisPage().show,
+            },
+            "chat": {
+                "title": "Bate-Papo",
+                "func": ChatPage().show,
+            },
+            "about": {
+                "title": "Sobre",
+                "func": AboutPage().show,
+            },
+        }
+        for page, value in self.__pages.items():
+            st.sidebar.button(
+                value["title"],
+                on_click=self.__set_page,
+                args=(page,),
+                use_container_width=True,
+            )
+        self.current_page_name = st.query_params.get("page", "home")
+
+    def run(self) -> None:
+        page = self.__pages.get(self.current_page_name)
+        func = page.get("func", None)
+        if func:
+            func()
+        else:
+            logger.error("Page function not found!")
+            st.error("Page function not found!")
+
+    @staticmethod
+    def __set_page(page_name: str) -> None:
+        st.query_params["page"] = page_name
