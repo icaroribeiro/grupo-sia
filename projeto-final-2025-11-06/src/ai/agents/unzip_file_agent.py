4121117fa0ea666a38ab1010aa393291a9560ac5
icaroribeiro <icaroribeiro@hotmail.com>
2025-11-02 08:56:49 -0300

    Add AI agents

---
+++ projeto-final-2025-11-06/src/ai/agents/unzip_file_agent.py
@@ -0,0 +1,18 @@
+from src.ai.agents.base_agent import BaseAgent
+
+
+class UnzipFileAgent(BaseAgent):
+    name: str = "unzip_file_agent"
+    prompt: str = """
+        ROLE:
+        - You are an unzip file agent.
+        
+        OBJECTIVE:
+        - You are responsible for unzipping a ZIP file based on the user's request.
+        
+        INSTRUCTIONS: 
+        - If the assigned task involves unzipping a ZIP file, use the `unzip_zip_file_tool` to unzip the ZIP file.
+        
+        CRITICAL RULES:
+        - **NEVER** return a conversational response, a summary, or route back to the supervisor without executing the task assigned.
+    """
